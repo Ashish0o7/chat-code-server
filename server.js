fb6606eaa77712000c4aa4533cdeffbe6e5c5914
aashish <75740364+Ashish0o7@users.noreply.github.com>
2023-05-27 20:47:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http, {
   cors: {
-    origin: 'http://localhost:3000', 
+    origin: 'https://code-editor-6rqa.onrender.com/', 
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type'],
     credentials: true,
