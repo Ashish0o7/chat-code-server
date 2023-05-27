89bdca0ec7869ac068280bac8c73d6eeb28927cc
aashish <75740364+Ashish0o7@users.noreply.github.com>
2023-05-28 00:12:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 const app = require('express')();
 const http = require('http').Server(app);
-const socketIO = require('socket.io');
-const cors = require('cors');
-const PORT = 8000;
-const rooms = {};
-const io = socketIO(http);
-// Enable CORS for all routes
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://code-editor-6rqa.onrender.com');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'https://code-editor-6rqa.onrender.com/', 
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
+    credentials: true,
+  },
 });
+const PORT = 8000; 
+const rooms = {};
 
 io.on('connection', (socket) => {
   console.log('A user connected');
