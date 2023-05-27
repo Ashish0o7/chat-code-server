d6c9184d46d867053a0a457c1efba0b6bc54f3ce
aashish <75740364+Ashish0o7@users.noreply.github.com>
2023-05-27 20:48:35 +0530

    Add files via upload

---
+++ server2.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const PORT = 8001;
+const cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
+
+// Featured codes data
+let featuredCodes = [];
+
+app.use(cors());
+app.use(express.json());
+
+// Get all featured codes
+app.get('/api/codes', (req, res) => {
+  res.json(featuredCodes);
+});
+
+// Submit a new code
+app.post('/api/codes', (req, res) => {
+  const { title, code } = req.body;
+  const newCode = { id: uuidv4(), title, code };
+
+  featuredCodes.push(newCode);
+    console.log("new code received");
+  res.setHeader('Content-Type', 'application/json');
+  res.status(201).json(newCode);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
