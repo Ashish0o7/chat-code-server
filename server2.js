7e522257e8a63b1d6c3c89e11861b7302473536c
aashish <75740364+Ashish0o7@users.noreply.github.com>
2024-04-10 13:18:28 +0530

    Update server2.js

--- server2.js
+++ server2.js
@@ -6,12 +6,9 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 const app = express();
-const corsOptions = {
-  origin: 'https://code-editor-6rqa.onrender.com',
-  optionsSuccessStatus: 200
-};
 
-app.use(cors(corsOptions));
+
+app.use(cors());
 
 app.use(express.json());
 
